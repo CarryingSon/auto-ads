2b375fb70b70172d29a26a2475dfd5c3e17cec51
Jure Križman <jk7348@student.uni-lj.si>
2026-03-20 15:02:31 +0100

    Increase landing navbar logo size

--- client/src/pages/landing.tsx
+++ client/src/pages/landing.tsx
@@ -182,7 +182,7 @@ export default function Landing() {
                 <img
                   src="/favicon.png"
                   alt="Auto-ads logo"
-                  className="w-8 h-8 object-contain"
+                  className="w-10 h-10 object-contain"
                 />
                 <span className="font-bold text-xl tracking-tight text-gray-900">Auto-ads</span>
               </div>
